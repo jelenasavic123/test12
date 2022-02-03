d3499c020b43a1c20048b675e4f3be34efea2b30
Jelena Savic <76621162+jelenasavic123@users.noreply.github.com>
2022-02-03 02:18:31 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+#Config file
+//Site Title
+$title = "Moko IPTV Player - IPTV Video Streaming Website";
+
+//Site URL
+$site_url = "https://projects.nodetent.com/mokoiptv/";
+
+//Site Description
+$description = "";
+
+//Watermark image
+$watermarkImage = "";
+
+//Watermark link
+$watermarkLink = "";
+
+//Google Analytics tag ID
+$gaCode = "UA-69771681-1";
+
+//Preroll Ads Video (.mp4)
+$prerollAds = "https://cdn.jsdelivr.net/gh/mokoshalb/clappr-ads/edumail.mp4";
+
+//Preroll Ads Time (seconds)
+$prerollTime = 5;
+
+//Preroll Ads External Link
+$prerollLink = "http://www.mokoshalb.com/buy-edu-mail/";
+?>
